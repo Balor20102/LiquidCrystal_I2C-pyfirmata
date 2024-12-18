db23f8e6d14d759593a7b0c0dbf28d80912f5bb6
DanielKool <daniel@familiekool.nl>
2024-12-18 11:28:06 +0100

    comments beteer

--- lcd_i2c.py
+++ lcd_i2c.py
@@ -15,11 +15,11 @@ message = "Hello, World!"
 # Convert the string to bytes
 message_bytes = [ord(char) for char in message]
 
-# Set the cursor to the first row position
+# Set the cursor to the first row position [x, y]
 arduino.send_sysex(LCD_SET_CURSOR, [0, 0])
 
-# Set the cursor to the second row position
-arduino.send_sysex(LCD_SET_CURSOR, [0, 0])
+# Set the cursor to the second row position [x, y]
+arduino.send_sysex(LCD_SET_CURSOR, [0, 1])
 
 # Send the command and the string to the Arduino
 arduino.send_sysex(LCD_PRINT, message_bytes)
